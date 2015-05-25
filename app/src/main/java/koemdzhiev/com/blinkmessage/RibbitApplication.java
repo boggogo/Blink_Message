c3ddd979ec158d9809e35c6d4d6750261d2164a5
boggogo <koemdjiev@gmail.com>
2015-05-25 15:14:38 +0100

    - everything works

--- app/src/main/java/koemdzhiev/com/blinkmessage/RibbitApplication.java
+++ app/src/main/java/koemdzhiev/com/blinkmessage/RibbitApplication.java
@@ -1,6 +1,7 @@
 package koemdzhiev.com.blinkmessage;
 
 import android.app.Application;
+import android.widget.Toast;
 
 import com.parse.Parse;
 import com.parse.ParseObject;
@@ -16,7 +17,9 @@ public class RibbitApplication extends Application {
         Parse.enableLocalDatastore(this);
         Parse.initialize(this, "sn7r0V8XOLdkVGS9TXplNBfackrO958mCro2PdEf", "xA8D4IejgaKzfWKk5rijHtD5Vw9vfQcWTfxiGMYp");
         ParseObject testObject = new ParseObject("TestObject");
+
         testObject.put("foo", "bar");
         testObject.saveInBackground();
+        Toast.makeText(getBaseContext(),"CONNECTED!",Toast.LENGTH_LONG).show();
     }
 }
