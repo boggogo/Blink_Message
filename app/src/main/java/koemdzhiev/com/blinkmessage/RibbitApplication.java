4a2b123c94d839d9ac677e88f49d7d31d99b1036
boggogo <koemdjiev@gmail.com>
2015-05-25 14:55:02 +0100

    included Parse TextObject

---
+++ app/src/main/java/koemdzhiev/com/blinkmessage/RibbitApplication.java
@@ -0,0 +1,22 @@
+package koemdzhiev.com.blinkmessage;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+/**
+ * Created by koemdzhiev on 25/05/2015.
+ */
+public class RibbitApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // Enable Local Datastore.
+        Parse.enableLocalDatastore(this);
+        Parse.initialize(this, "sn7r0V8XOLdkVGS9TXplNBfackrO958mCro2PdEf", "xA8D4IejgaKzfWKk5rijHtD5Vw9vfQcWTfxiGMYp");
+        ParseObject testObject = new ParseObject("TestObject");
+        testObject.put("foo", "bar");
+        testObject.saveInBackground();
+    }
+}
