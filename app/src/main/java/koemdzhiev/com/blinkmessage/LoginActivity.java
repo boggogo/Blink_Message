d77e99a96b0647afbe728b04813d961661495d1e
boggogo <koemdjiev@gmail.com>
2015-06-06 20:55:30 +0100

    customed progress bar color - added

--- app/src/main/java/koemdzhiev/com/blinkmessage/LoginActivity.java
+++ app/src/main/java/koemdzhiev/com/blinkmessage/LoginActivity.java
@@ -1,12 +1,15 @@
 package koemdzhiev.com.blinkmessage;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.parse.LogInCallback;
@@ -16,16 +19,19 @@ import com.parse.ParseUser;
 
 public class LoginActivity extends Activity {
     protected TextView mSignUpTextView;
-
     protected EditText mUsername;
     protected EditText mPassword;
     protected Button mLoginButton;
+    protected ProgressBar mProgressBar;
 
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+        mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
+        mProgressBar.setVisibility(View.INVISIBLE);
+        mProgressBar.setIndeterminateDrawable(getDrawable(R.drawable.progress));
         mSignUpTextView = (TextView) findViewById(R.id.signUpText);
         mSignUpTextView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,6 +47,8 @@ public class LoginActivity extends Activity {
         mLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                mProgressBar.setVisibility(View.VISIBLE);
+
                 String username = mUsername.getText().toString();
                 String password = mPassword.getText().toString();
 
@@ -48,6 +56,7 @@ public class LoginActivity extends Activity {
                 password = password.trim();
 
                 if(username.isEmpty() || password.isEmpty()){
+                    mProgressBar.setVisibility(View.INVISIBLE);
                     AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
                     builder.setMessage(R.string.login_error_message);
                     builder.setTitle(R.string.login_error_title);
@@ -62,11 +71,13 @@ public class LoginActivity extends Activity {
                         public void done(ParseUser user, ParseException e) {
                             if(e == null){
                                 //Success!
+                                mProgressBar.setVisibility(View.INVISIBLE);
                                 Intent intent = new Intent(LoginActivity.this,MainActivity.class);
                                 intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
                                 startActivity(intent);
                             }else{
+                                mProgressBar.setVisibility(View.INVISIBLE);
                                 AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
                                 builder.setMessage(R.string.login_error_message);
                                 builder.setTitle(R.string.login_error_title);
