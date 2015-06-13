07b7a27a75ddc40685675d1f12e83d1e691ab87c
boggogo <koemdjiev@gmail.com>
2015-06-13 18:39:19 +0100

    styling the inbox viewlist. Refactoring the created at textview

--- app/src/main/java/koemdzhiev/com/blinkmessage/adapters/MessageAdapter.java
+++ app/src/main/java/koemdzhiev/com/blinkmessage/adapters/MessageAdapter.java
@@ -1,6 +1,7 @@
 package koemdzhiev.com.blinkmessage.adapters;
 
 import android.content.Context;
+import android.text.format.DateUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,10 +11,11 @@ import android.widget.TextView;
 
 import com.parse.ParseObject;
 
+import java.util.Date;
 import java.util.List;
 
-import koemdzhiev.com.blinkmessage.utils.ParseConstants;
 import koemdzhiev.com.blinkmessage.R;
+import koemdzhiev.com.blinkmessage.utils.ParseConstants;
 
 /**
  * Created by koemdzhiev on 03/06/2015.
@@ -45,14 +47,20 @@ public class MessageAdapter extends ArrayAdapter<ParseObject> {
         }
 
         ParseObject message = mMessages.get(position);
+        Date createdAt = message.getCreatedAt();
+
+        Long now = new Date().getTime();
+        String convertedData = DateUtils.getRelativeTimeSpanString(
+                createdAt.getTime(),
+                now,
+                DateUtils.SECOND_IN_MILLIS).toString();
+
         if(message.getString(ParseConstants.KEY_FILE_TYPE).equals(ParseConstants.TYPE_IMAGE)){
-            holder.iconImageView.setImageResource(R.drawable.ic_action_picture);
+            holder.iconImageView.setImageResource(R.mipmap.ic_picture);
         }else{
-            holder.iconImageView.setImageResource(R.drawable.ic_action_play_over_video);
+            holder.iconImageView.setImageResource(R.mipmap.ic_video);
         }
-        String createdAt = message.getCreatedAt().toString();
-        String createdAtSubStr = createdAt.substring(0,19);
-        holder.createdAtLabel.setText(createdAtSubStr);
+        holder.createdAtLabel.setText(convertedData);
         //Log.i(MessageAdapter.class.getSimpleName(),message.getCreatedAt().toString());
         holder.nameLabel.setText(message.getString(ParseConstants.KEY_SENDER_NAME));
 
