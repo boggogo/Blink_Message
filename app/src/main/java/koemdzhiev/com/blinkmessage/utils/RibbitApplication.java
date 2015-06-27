618959f8146b2521e5657ac361a57f632a444b3b
boggogo <koemdjiev@gmail.com>
2015-06-27 08:12:48 +0100

    adding push notifications

--- app/src/main/java/koemdzhiev/com/blinkmessage/utils/RibbitApplication.java
+++ app/src/main/java/koemdzhiev/com/blinkmessage/utils/RibbitApplication.java
@@ -3,6 +3,7 @@ package koemdzhiev.com.blinkmessage.utils;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseInstallation;
 
 /**
  * Created by koemdzhiev on 25/05/2015.
@@ -14,6 +15,7 @@ public class RibbitApplication extends Application {
         // Enable Local Datastore.
         Parse.enableLocalDatastore(this);
         Parse.initialize(this, "sn7r0V8XOLdkVGS9TXplNBfackrO958mCro2PdEf", "xA8D4IejgaKzfWKk5rijHtD5Vw9vfQcWTfxiGMYp");
+        ParseInstallation.getCurrentInstallation().saveInBackground();
        
     }
 }
